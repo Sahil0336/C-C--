7290565cb263aac6e1c1ee42f3423b8c0ffaaa2b
Sahil0336 <mohamedsahil0336@outlook.com>
2022-11-08 11:07:35 +0530

    sum

--- test.c
+++ test.c
@@ -1,14 +1,18 @@
 // Find the sum of all elements in an array of integers
 #include <stdio.h>
+#define n 5
 int main()
 {
-    int a[5], i;
-    for (i = 0; i < 5; i++)
+    int a[n], i, p = 0;
+    for (i = 0; i < n; i++)
     {
         printf("\n Enter next no:");
         scanf("%d", &a[i]);
     }
-    for (i = 0; i < 5; i++)
-        printf("%d, ", a[i]);
+    for (i = 0; i < n; i++)
+    {
+        p = p + a[i];
+    }
+    printf("%d, ", p);
     return 0;
 }
