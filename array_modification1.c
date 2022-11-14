8e4b0d1fe4127d84c8d342af0ed777f605c9bc1b
Sahil0336 <mohamedsahil0336@outlook.com>
2022-11-14 23:56:07 +0530

    rearrange

--- array_modification1.c
+++ array_modification1.c
@@ -12,9 +12,9 @@ int main()
     for (i = p - 1; i < n - 1; i++)
         a[i] = a[i + 1];
     a[i] = 0;
-    printf("\nModified array: ");
+    printf("------------------------------\nModified array: ");
     for (i = 0; i < n; i++)
 
-        printf("%d", a[i]);
+        printf(" %d ", a[i]);
     return 0;
 }
