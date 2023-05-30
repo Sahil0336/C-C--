c9da86ae80d33318a485199dd63c9fd210a4257e
Sahil0336 <mohamedsahil0336@gmail.com>
2023-05-31 02:01:51 +0530

    number system

---
+++ DecimalToAnybase.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+char a[]="0123456789ABCDEF";
+int main()
+{
+    int n,b;
+    int anybase(int,int);
+    printf("\nEnter Number: ");
+    scanf("%d",&n);
+    printf("Enter base to convert: ");
+  scanf("%d",&b);
+  printf("The result: ");
+  anybase(n,b);
+}
+int anybase(int n, int b)
+{
+    if(n)
+    {
+        anybase(n/b,b);
+        printf("%c",a[n%b]);
+    }
+}
+
+
