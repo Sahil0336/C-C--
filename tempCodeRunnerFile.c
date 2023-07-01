7de4a666f406d74853b4259bc3def1e8f289b283
Sahil0336 <mohamedsahil0336@gmail.com>
2023-07-01 10:41:27 +0530

    Pattern Update

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+	int n, i, j, num = 1, gap;
+printf("Enter the number:  ");
+scanf("%d",&n);
+gap = n-1;
+	for ( j = 1 ; j <= n ; j++ )
+	{
+		num = j;
+		for ( i = 1 ; i <= gap ; i++ )
+			printf(" ");
+		gap --;
+		for ( i = 1 ; i <= j ; i++ )
+		{
+			printf("%d", num);
+			num++;
+		}
+		num--;
+		num--;
+		for ( i = 1 ; i < j ; i++)
+		{
+			printf("%d", num);
+			num--;
+		}
+		printf("\n");
+	}
+	return 0;
 }
