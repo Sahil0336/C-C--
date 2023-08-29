55ff004e2da65490f6b9ed6eefda37828a5a4f14
Sahil0336 <mohamedsahil0336@gmail.com>
2023-08-29 21:56:19 +0530

    22111212

--- Practice/Num_dec_to_anybase.c
+++ Practice/Num_dec_to_anybase.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,b;
-    int anybase(int,int);
+    int n, b;
+    int anybase(int, int);
     printf("\nEnter any decimal number: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("\nEnter base value: ");
-    scanf("%d",&b);
+    scanf("%d", &b);
     printf("\n\nThe Result: ");
-    anybase(n,b);
+    anybase(n, b);
 }
-int anybase(int n,int b)
+int anybase(int n, int b)
 {
     int p;
-    if(n)
+    if (n)
     {
-        anybase(n/b,b);
-    }
-    if(n%b >9)
-    printf("%c",55+(n%b));
+        anybase(n / b, b);
+    
+    if (n % b > 9)
+        printf("%c", 55 + (n % b));
     else
-    printf("%d",n%b);
+        printf("%d", n % b);
+    }
 }
