88084ca945eff625be1ab503c4607adfdcc7fd5f
Sahil0336 <mohamedsahil0336@gmail.com>
2023-10-09 02:13:25 +0530

    None

---
+++ true.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+   int arr(3);
+   int i,n;
+   printf("Enter number of elements: ");
+   scanf("%d",&n);
+   for ( i = 0; i < n; i++)
+   {
+    
+    scanf("%d",&arr[i]);
+   }
+   for ( i = 0; i < n; i++)
+   {
+    if (/* condition */)
+    {
+        /* code */
+    }
+    
+   }
+      
+}
